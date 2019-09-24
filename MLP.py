66e05b03b408734bb07673c55cd8bc9b991c9afd
Jobson Mateus <jobsonmateus@MacBook-Air-de-Jobson-2.local>
2019-09-24 00:40:35 -0300

    Implementação do offline

--- MLP.py
+++ MLP.py
@@ -7,7 +7,6 @@ import numpy
 random.seed(2)
 # General Functions
 def rand(a, b):
-
     return random.uniform(a, b)
 
 def funcao_ativacao_tangente_hiperbolica(x): # funcao tangente hiperbolica
@@ -28,12 +27,22 @@ class Rede:
         self.qtd_neuronios_camada_escondida = qtd_neuronios_camada_escondida # +1 -> bias
         self.qtd_neuronios_camada_saida = qtd_neuronios_camada_saida
 
+        # Init Online ------------------------------------------------------
         self.I_camada_escondida = numpy.ones(self.qtd_neuronios_camada_escondida)
         self.I_camada_de_saida = numpy.ones(self.qtd_neuronios_camada_saida)
 
         self.entrada = numpy.ones(self.qtd_entrada)
         self.saida_camada_escondida = numpy.ones(self.qtd_neuronios_camada_escondida)
         self.saida_camada_de_saida = numpy.ones(self.qtd_neuronios_camada_saida)
+        # -------------------------------------------------------------------
+
+        # Init Offline ------------------------------------------------------
+        self.I_camada_escondida_off = []
+        self.I_camada_de_saida_off = []
+
+        self.saida_camada_escondida_off = []
+        self.saida_camada_saida_off = []
+        # -------------------------------------------------------------------
 
         # self.entrada[self.qtd_entrada - 1] = -1 # ENTRADA BIAS CAMADA ESCONDIDA
         # self.saida_camada_escondida[self.qtd_neuronios_camada_escondida - 1] = -1 # ENTRADA BIAS CAMADA DE SAIDA
@@ -70,6 +79,55 @@ class Rede:
                 self.pesos_camada_saida[i][j] = rand(-0.5, 0.5)
                 print("{}".format(self.pesos_camada_saida[i][j]))
 
+    def treinar(self, entradas_saidas):
+
+        epocas = 0
+
+        eqm = 0
+        erro_anterior = 1000
+        while (epocas < self.max_interacoes and abs(eqm - erro_anterior) > self.precisao):
+
+            # random.shuffle(entradas_saidas)
+
+            erro_anterior = eqm
+            erro = 0
+            for p in entradas_saidas:
+                entradas = p[0]
+                saidas_desejadas = p[1]
+
+                self.forward(entradas)
+                erro += self.backward(saidas_desejadas)
+            eqm = erro / len(entradas_saidas)
+            # if epocas % 100 == 0:
+            print("Treinando epoca: {} / erro: {}".format(epocas, eqm - erro_anterior))
+
+            epocas += 1
+
+        print("Epocas: {}".format(epocas))
+        # print("EQM: {} / EQM ANTERIOR: {}".format(eqm, erro_anterior))
+
+    def teste(self, entradas_saidas, saida_desejada):
+        print("--------------------------------------")
+        print("Saida desejada:")
+
+        for index, saida in enumerate(saida_desejada):
+            print("{}: {}".format(index, saida))
+
+        print("Saída encontrada:")
+        qtd_acertos = 0
+        for index, p in enumerate(entradas_saidas):
+            array = self.forward(p[0])
+            erro = False
+            for i in range(len(array)):
+                array[i] = round(array[i], 1)
+                if ((array[i] < 0.8 and saida_desejada[index][i] == 1) or (
+                        array[i] > -0.8 and saida_desejada[index][i] == -1)) and (not erro):
+                    erro += True
+            if not erro:
+                qtd_acertos += 1
+            print("{}: {}".format(index, array))
+        print("{}%".format(int(qtd_acertos / len(saida_desejada) * 100)))
+
     # FORWARD FUNCTIONS
     def forward(self, entradas):
         for i in range(self.qtd_entrada):
@@ -86,8 +144,13 @@ class Rede:
             for i in range(self.qtd_entrada):
                 somatorio = somatorio + self.entrada[i] * self.pesos_camada_escondida[i][j]
             somatorio = somatorio + (-1 * self.pesos_camada_escondida[self.qtd_entrada][j])
+
             self.I_camada_escondida[j] = somatorio
+
+
             self.saida_camada_escondida[j] = funcao_ativacao_tangente_hiperbolica(somatorio)
+        self.I_camada_escondida_off.append(self.I_camada_escondida)
+        self.saida_camada_escondida_off.append(self.saida_camada_escondida)
 
     def run_camada_de_saida(self):
         for j in range(self.qtd_neuronios_camada_saida):
@@ -95,8 +158,12 @@ class Rede:
             for i in range(self.qtd_neuronios_camada_escondida):
                 somatorio = somatorio + self.saida_camada_escondida[i] * self.pesos_camada_saida[i][j]
             somatorio = somatorio + (-1 * self.pesos_camada_saida[self.qtd_neuronios_camada_escondida][j])
+
             self.I_camada_de_saida[j] = somatorio
+
             self.saida_camada_de_saida[j] = funcao_ativacao_tangente_hiperbolica(somatorio)
+        self.I_camada_de_saida_off.append(self.I_camada_de_saida)
+        self.saida_camada_saida_off.append(self.saida_camada_de_saida)
 
     # BACKWARD FUNCTIONS
     def backward(self, saidas_desejadas):
@@ -159,52 +226,101 @@ class Rede:
                 else:
                     self.pesos_camada_escondida[i][j] += (self.taxa_aprendizado * residuos_escondida[j] * -1)
 
-    def teste(self, entradas_saidas, saida_desejada):
-        print("--------------------------------------")
-        print("Saida desejada:")
-
-        for index, saida in enumerate(saida_desejada):
-            print("{}: {}".format(index, saida))
-
-        print("Saída encontrada:")
-        qtd_acertos = 0
-        for index, p in enumerate(entradas_saidas):
-            array = self.forward(p[0])
-            erro = False
-            for i in range(len(array)):
-                array[i] = round(array[i], 1)
-                if ((array[i] < 0.8 and saida_desejada[index][i] == 1) or (array[i] > -0.8 and saida_desejada[index][i] == -1)) and (not erro):
-                    erro += True
-            if not erro:
-                qtd_acertos += 1
-            print("{}: {}".format(index, array))
-        print("{}%".format(int(qtd_acertos/len(saida_desejada) * 100)))
-
-    def treinar(self, entradas_saidas):
+    def treinar_offline(self, entradas_saidas):
 
         epocas = 0
 
-        eqm = 1000
-        erro_anterior = 0
+        eqm = 0
+        erro_anterior = 1000
 
         while (epocas < self.max_interacoes and abs(eqm - erro_anterior) > self.precisao):
 
-            # random.shuffle(entradas_saidas)
-            eqm = 0
+
             erro_anterior = eqm
             erro = 0
-            for p in entradas_saidas:
-
-                entradas = p[0]
-                saidas_desejadas = p[1]
-
+            self.saida_camada_escondida_off = []
+            self.saida_camada_saida_off = []
+            for amostra in entradas_saidas:
+                entradas = amostra[0]
                 self.forward(entradas)
-                erro += self.backward(saidas_desejadas)
+
+            for k, amostra in enumerate(entradas_saidas):
+                entradas = amostra[0]
+                saidas_desejadas = amostra[1]
+
+                IE = self.I_camada_escondida_off[k]
+                YE = self.saida_camada_escondida_off[k]
+
+                IS = self.I_camada_de_saida_off[k]
+                YS = self.saida_camada_saida_off[k]
+
+                erro += self.backward_offline(saidas_desejadas, IE, YE, IS, YS, entradas)
             eqm = erro/len(entradas_saidas)
-            if epocas % 100 == 0:
-                print("Treinando epoca: {} / erro: {}".format(epocas, eqm))
+            # if epocas % 100 == 0:
+            print("Treinando epoca: {} / erro: {}".format(epocas, eqm))
 
             epocas += 1
 
         print("Epocas: {}".format(epocas))
-        # print("EQM: {} / EQM ANTERIOR: {}".format(eqm, erro_anterior))
+
+
+    def backward_offline(self, saidas_desejadas, IE, YE, IS, YS, ent):
+        # CAMADA DE SAIDA - RESIDUO
+        residuos_saida = self.get_residuos_camada_saida_off(saidas_desejadas, IS, YS)
+
+        # CAMADA DE ESCONDIDA - RESIDUO
+        residuos_escondida = self.get_residuos_camada_escondida_off(residuos_saida, IE)
+
+        # CAMADA DE SAIDA - AJUSTE
+        self.ajustar_pesos_camada_saida_off(residuos_saida, YE)
+
+        # CAMADA DE ESCONDIDA - AJUSTE
+        self.ajustar_pesos_camada_escondida_off(residuos_escondida, ent)
+
+        # calculando erro
+        erro = 0
+        for i in range(len(saidas_desejadas)):
+            erro += ((saidas_desejadas[i] - self.saida_camada_de_saida[i]) ** 2)/2
+        return erro
+
+    def get_residuos_camada_saida_off(self, respostas, I, Y):
+
+        residuos_saida = numpy.zeros(self.qtd_neuronios_camada_saida)
+        erro = 0
+
+        for i in range(self.qtd_neuronios_camada_saida):
+            erro = respostas[i] - Y[i]
+            residuos_saida[i] = derivada_funcao_ativacao(I[i]) * erro
+
+        return residuos_saida
+
+    def get_residuos_camada_escondida_off(self, residuos_saida, I):
+        residuos_escondida = numpy.zeros(self.qtd_neuronios_camada_escondida)
+
+        for i in range(self.qtd_neuronios_camada_escondida):
+            erro = 0
+            for j in range(self.qtd_neuronios_camada_saida):
+                erro += residuos_saida[j] * self.pesos_camada_saida[i][j]
+            residuos_escondida[i] = derivada_funcao_ativacao(I[i]) * erro
+
+        return residuos_escondida
+
+    def ajustar_pesos_camada_saida_off(self, residuos_saida, Y):
+
+        for i in range(self.qtd_neuronios_camada_escondida):
+            for j in range(self.qtd_neuronios_camada_saida):
+                if i != self.qtd_entrada:
+                    self.pesos_camada_saida[i][j] += (self.taxa_aprendizado * residuos_saida[j] * Y[i])
+                else:
+                    self.pesos_camada_escondida[i][j] += (self.taxa_aprendizado * residuos_saida[j] * -1)
+
+
+    def ajustar_pesos_camada_escondida_off(self, residuos_escondida, ent):
+
+        for i in range(self.qtd_entrada + 1):
+            for j in range(self.qtd_neuronios_camada_escondida):
+                if i != self.qtd_entrada:
+                    self.pesos_camada_escondida[i][j] += (self.taxa_aprendizado * residuos_escondida[j] * ent[i])
+                else:
+                    self.pesos_camada_escondida[i][j] += (self.taxa_aprendizado * residuos_escondida[j] * -1)
+
--- main.py
+++ main.py
@@ -15,13 +15,15 @@ for index, e in enumerate(entradas):
     entradas_saidas[index].append(entradas[index])
     entradas_saidas[index].append(respostas[index])
 
-TAXA_DE_APRENDIZADO = 0.1
-PRECISAO = 0.001
+TAXA_DE_APRENDIZADO = 0.05
+PRECISAO = 0.00001
 MAX_ITERACOES = 50000
 QTD_ENTRADAS = len(entradas_saidas[0][0])
 QTD_NEURONIOS_CAMADA_ESCONDIDA = 2
 QTD_SAIDAS = len(entradas_saidas[0][1])
+ONLINE = True
 
+print("MODE: {}".format("ONLINE" if ONLINE else "OFFLINE"))
 print("TAXA_DE_APRENDIZADO: {}".format(TAXA_DE_APRENDIZADO))
 print("MAX_ITERACOES: {}".format(MAX_ITERACOES))
 print("QTD_ENTRADAS: {}".format(QTD_ENTRADAS))
@@ -30,6 +32,9 @@ print("QTD_SAIDAS: {}".format(QTD_SAIDAS))
 
 rede = Rede(QTD_ENTRADAS, QTD_NEURONIOS_CAMADA_ESCONDIDA, QTD_SAIDAS, TAXA_DE_APRENDIZADO, MAX_ITERACOES, PRECISAO)
 
-rede.treinar(entradas_saidas)
+if ONLINE:
+    rede.treinar(entradas_saidas)
+else:
+    rede.treinar_offline(entradas_saidas)
 
 rede.teste(entradas_saidas, respostas)
